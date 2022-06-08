2f5b00d1b3cb23cdca2c9ff3c56736f86f3b3fa3
HIran M <45740292+hiranmohanan@users.noreply.github.com>
2022-06-08 22:45:02 +0530

    splash about

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:attendance_montior/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
@@ -10,22 +11,57 @@ class SplashScreen extends StatelessWidget {
         backgroundColor: Colors.transparent,
       ),
       bottomNavigationBar: BottomAppBar(
-        color: Colors.transparent,
+        color: AppColors.blackGlaze,
         child: Container(
           height: 350,
           decoration: const BoxDecoration(
             color: Colors.white70,
-            borderRadius: BorderRadius.only(topLeft: Radius.circular(50)),
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(60), topRight: Radius.circular(60)),
           ),
           child: Stack(
             children: [
-              Text("UCEK"),
-              Container(
-                  decoration: BoxDecoration(
-                      color: Colors.white70,
-                      borderRadius: BorderRadius.all(Radius.circular(50))),
-                  child: IconButton(
-                      onPressed: null, icon: Icon(Icons.arrow_forward_ios))),
+              Positioned(
+                  left: 170,
+                  right: 170,
+                  top: 20,
+                  child: Text(
+                    "UCEK",
+                    style: TextStyle(
+                        color: AppColors.redDark,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  )),
+              Padding(
+                padding: const EdgeInsets.only(top: 80, left: 28, right: 20),
+                child: RichText(
+                    text: TextSpan(
+                        text: "HI there,  ",
+                        style: TextStyle(
+                            color: AppColors.orangeLight, fontSize: 23),
+                        children: <TextSpan>[
+                      TextSpan(
+                        text: "welcome to UCEK Buzz",
+                        style:
+                            TextStyle(color: AppColors.cyanDark, fontSize: 20),
+                      ),
+                      TextSpan(
+                        text: "\nKeep updated about your attendence and class ",
+                        style:
+                            TextStyle(color: AppColors.cyanDark, fontSize: 20),
+                      ),
+                    ])),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 250, left: 182, right: 20),
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white70,
+                        borderRadius: BorderRadius.all(Radius.circular(50))),
+                    child: IconButton(
+                        onPressed: null, icon: Icon(Icons.arrow_forward_ios))),
+              ),
             ],
           ),
         ),
