a7d2430eebc6690f088fba5061c6e5a462060439
Abhiram S <4bh1ram@gmail.com>
2022-06-08 22:12:08 +0530

    button added

--- lib/screens/mark_attendance_screen.dart
+++ lib/screens/mark_attendance_screen.dart
@@ -20,38 +20,42 @@ class MarkAttendanceScreen extends StatelessWidget {
       body: Column(
         children: [
           Container(
-              width: 100.w,
-              margin: EdgeInsets.only(bottom: 10),
-              padding: EdgeInsets.only(bottom: 20),
+            width: 100.w,
+            margin: EdgeInsets.only(bottom: 10),
+            padding: EdgeInsets.only(bottom: 20),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
-              
               children: [
-                 Text(
+                Text(
                   "S5 CSE",
                   style: TextStyle(
-                    color: AppColors.cyanLight,
-                      fontSize: 34, letterSpacing: 0.0, fontWeight: FontWeight.w400),
+                      color: AppColors.cyanLight,
+                      fontSize: 34,
+                      letterSpacing: 0.0,
+                      fontWeight: FontWeight.w400),
                 ),
                 SizedBox(
                   width: 20.w,
                   child: Column(
                     children: [
-                        Text(
-                      "0",
+                      Text(
+                        "0",
                         style: TextStyle(
-                          color: AppColors.cyanNormal,
-                            fontSize:34, letterSpacing: 0.25, fontWeight: FontWeight.w400),
+                            color: AppColors.cyanNormal,
+                            fontSize: 34,
+                            letterSpacing: 0.25,
+                            fontWeight: FontWeight.w400),
                       ),
                       Divider(
                         color: AppColors.redNormal,
-                    
                       ),
                       Text(
-                      "45",
+                        "45",
                         style: TextStyle(
-                            fontSize: 24, letterSpacing: 0.00, fontWeight: FontWeight.w400),
+                            fontSize: 24,
+                            letterSpacing: 0.00,
+                            fontWeight: FontWeight.w400),
                       ),
                     ],
                   ),
@@ -71,10 +75,33 @@ class MarkAttendanceScreen extends StatelessWidget {
           ),
           Expanded(
             child: ListView.builder(
-                itemCount: 45,
+                itemCount: 45 + 1,
                 itemBuilder: (cntx, index) {
                   final TextEditingController textController =
                       TextEditingController();
+                  if (index == 45 ) {
+                    return Row(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 5.0.h),
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Upload',
+                                style: TextStyle(
+                                    fontSize: 17, fontWeight: FontWeight.w500),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                  padding: EdgeInsets.symmetric(vertical: 4.w),
+                                  primary: AppColors.cyanDark,
+                                  shape: const StadiumBorder()),
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  }
                   return Container(
                     child: AppFormField(
                       controller: textController,
@@ -83,7 +110,6 @@ class MarkAttendanceScreen extends StatelessWidget {
                   );
                 }),
           ),
-          
         ],
       ),
     );
