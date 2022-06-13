1382d486111823d6014fb592e13cf3643e387c55
HIran M <45740292+hiranmohanan@users.noreply.github.com>
2022-06-13 22:16:28 +0530

    i am done

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -3,6 +3,7 @@ import 'package:attendance_montior/screens/attendece_view_screen.dart';
 import 'package:attendance_montior/screens/home_screen.dart';
 import 'package:attendance_montior/screens/login.dart';
 import 'package:attendance_montior/screens/mark_attendance_screen.dart';
+import 'package:attendance_montior/screens/signup.dart';
 import 'package:attendance_montior/screens/syllabus_screen.dart';
 import 'package:get/get.dart';
 
